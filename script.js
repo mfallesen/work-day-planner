19d7ceefa24f3b45f6b4ec87a7b1a08d9872d408
Mike Fallesen <mfallesen2010@gmail.com>
2020-09-18 23:25:56 -0700

    implement clear schedule button almost finished with past,present future

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ let container = $(".container");
 let saveToLocal = $(".saveBtn");
 
 const workHours = ["9:00 am", "10:00 am", "11:00 am", "12:00 pm", "1:00 pm", "2:00 pm", "3:00 pm", "4:00 pm", "5:00 pm",];
+const currentHour = moment().format("H")
 
 
 
-console.log(now.format());
-console.log(workHours);
 
-console.log(now.format("h A"));
+
+
 function printWorkDay(hour) {
-
+    //creates the work rows
     for (var i = 0; i < hour.length; i++) {
 
         let hourRow = $("<div></div>");
@@ -24,7 +24,8 @@ function printWorkDay(hour) {
 
         hourRow.attr("class", "row time-block");
         workHour.attr("class", "col-md-2 hour");
-        workHour.attr("id", "hour-" + i);
+        workHour.attr("id", "hour-" + (9 + i));
+
         workHour.text(hour[i]);
         tasks.attr("class", "col-md-8 past");
         tasks.attr("id", "text-" + i);
@@ -37,26 +38,28 @@ function printWorkDay(hour) {
         hourRow.append(workHour, tasks, save);
         save.append(savIco);
     }
-    // IF statement for the tense changes
-    
+
+
+    for (var j = 0; j < 9; j++) {
+        let workID = $("#hour-" + (9 + j));
+        let singleHour = workID[0].id.replace("hour-", "");
+        let taskMod = $("#text-" + j);
+        console.log(taskMod);
+        if (currentHour === singleHour) {
+            taskMod.addClass("present");
+        } else if (currentHour > singleHour) {
+            taskMod.addClass("past");
+        } else {
+            taskMod.addClass("future");
+        }
+    }
 }
-
-printWorkDay(workHours);
-
+//gives the current date and time
 function currentDay() {
     $("#currentDay").text(now.format("LLLL"));
 }
 
-currentDay();
-
-$(".saveBtn").click(saveTask);
-// console.log();
-// function saveBtnListeners() {
-//     for (var i = 0; i < saveToLocal.length; i++) {
-//         $("#save-" + i).click(saveTask);
-//     }
-// }
-
+//saves to local storage
 function saveTask(event) {
     console.log(event)
     let idx = event.target.id.replace("save-", "")
@@ -64,35 +67,17 @@ function saveTask(event) {
     localStorage.setItem(`Task${idx}`, textInput);
 }
 
+// Event Handlers
+//saves tasks to local storage
+$(".saveBtn").click(saveTask);
+//clears local Storage and  reloads page.
+$("#scheduleClear").click(function() {
+    localStorage.clear();
+    location.reload();
+})
 
 
-
-
-
-// TODO: function pastCheck() {
-//     if (workHour.text() >= )
-// }
-
-// TODO: How can I compare the hours of my rows, to the current time? (Use moment.js get the current time and compare to the time that is being assigned to the row)
-
-// TODO:  give the elements the classes that they need e.g using an if statment we can check using monet.js to see if the hour that we're looping ove is past the current hour if so give the text area the class of past
-
-// TODO: Create click event listener for my buttons 
-
-// TODO:  Grab the value of the text are and save it to a var (I nedd to be able to save the text from the text area that is in the same row as my button)
-
-// TODO: using localStorage.setItem save the text to local storage
-
-// TODO: retrieve the data from local storage using localStorage.getItem and show them back on the text area that they belong to (How can i know what text from local storage goes to what text area?)
-
-// localStorage.setItem("test", "My first note" )
-// localStorage.setItem("test1", "My second note" )
-
-
-// var text1FromLocalStorage= localStorage.getItem("test")
-// $(".first").text(text1FromLocalStorage)
-
-// var text2FromLocalStorage= localStorage.getItem("test1")
-
-// $(".second").text(text2FromLocalStorage)
+//initialize Functions
+printWorkDay(workHours);
+currentDay();
 
